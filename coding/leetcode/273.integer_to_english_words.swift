12a94583162266cd7849bde86d8e97ec80203032
Paul Aguilar <aguilarpgc@gmail.com>
2021-02-28 22:32:18 -0500

    improved 273

--- coding/leetcode/273.integer_to_english_words.swift
+++ coding/leetcode/273.integer_to_english_words.swift
@@ -1,64 +1,67 @@
 class Solution {
+    let units = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+    let tensOne = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"] 
+    let tens = ["Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+    let levels = ["Thousand", "Million", "Billion"]
+
+    func hundredFormat(remainder: Int) -> String? {
+        guard remainder >= 100 && remainder <= 999 else {
+            return nil
+        }
+        let hundred = remainder / 100
+        return units[hundred - 1] + " Hundred"
+    }
+    
+    func tensFormat(remainder: Int) -> String? {
+        let tensNumber = remainder % 100
+        switch tensNumber {
+        case 1..<10:
+            return units[tensNumber - 1]
+        case 10..<20: 
+            return tensOne[tensNumber - 10]
+        case 20...99:
+            let tenIndex = (tensNumber / 10) - 2
+            let unit = remainder % 10
+            return tens[tenIndex] + (unit > 0 ? " " + units[unit - 1] : "")
+        default:
+            return nil
+        }
+    }
+    
     func numberToWords(_ input: Int) -> String {
-         // 1 - 9
-        var units = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
-        // 10 - 19
-        var tensOne = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"] 
-        var tens = ["Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"] // 2 - 9
-        var levels = ["Thousand", "Million", "Billion"]
-        
         guard input != 0 else {
             return "Zero"
         }
         
-        func tensFormat(remainder: Int) -> String? {
-            let tensNumber = remainder % 100
-            switch tensNumber {
-            case 1..<10:
-                return units[tensNumber - 1]
-            case 10..<20: 
-                return tensOne[tensNumber - 10]
-            case 20...99:
-                let hundred = (tensNumber / 10) - 2
-                let unit = remainder % 10
-                return tens[hundred] + (unit > 0 ? " " + units[unit - 1] : "")
-            default:
-                return nil
-            }
-        }
-        
         var num = input
-        let mod = 1000
         var countLevels = 0
         var output: [String] = []
         
         while num > 0 {
-            let rest = num % mod
+            let rest = num % 1000
+            var tempOutput: String?
             
-            var outputLevel: String?
-            
-            if rest >= 100 && rest <= 999 {   
-                let hundred = rest / 100
-                outputLevel = (units[hundred - 1] + " Hundred")
+            if let format = hundredFormat(remainder: rest) {
+                tempOutput = format
             }
             
             if let tensFormat = tensFormat(remainder: rest) {
-                if let format = outputLevel {
-                    outputLevel = format + " " + tensFormat
+                if let format = tempOutput {
+                    tempOutput = format + " " + tensFormat
                 } else {
-                    outputLevel = tensFormat   
+                    tempOutput = tensFormat   
                 }
             }
             
-            if let format = outputLevel, countLevels != 0 && rest != 0 {
-                outputLevel = format + " " + levels[countLevels - 1]
+            if let format = tempOutput, countLevels != 0 && rest != 0 {
+                tempOutput = format + " " + levels[countLevels - 1]
             }
             
-            if let format = outputLevel {
+            if let format = tempOutput {
                 output.append(format)   
             }
             
-            num /= mod
+            num /= 1000
             countLevels += 1
         }
         
