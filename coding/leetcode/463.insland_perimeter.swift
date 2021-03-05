07cbaa2dca8a3398b2d06f913e028f9176fbd52b
Paul Aguilar <aguilarpgc@gmail.com>
2021-03-04 23:38:47 -0500

    updated island perimeter

--- coding/leetcode/463.insland_perimeter.swift
+++ coding/leetcode/463.insland_perimeter.swift
@@ -1,23 +1,15 @@
 class Solution {
     func islandPerimeter(_ grid: [[Int]]) -> Int {
         var result = 0
-        let rows = grid.count
         for i in 0..<grid.count {
-            let columns = grid[i].count
-            for j in 0..<columns { 
+            for j in 0..<grid[i].count { 
                 if grid[i][j] == 1 {
                     result += 4
                     if i - 1 >= 0 && grid[i-1][j] == 1 {
-                        result -= 1
+                        result -= 2
                     }
                     if j - 1 >= 0 && grid[i][j-1] == 1 {
-                        result -= 1
-                    }
-                    if i + 1 < rows && grid[i+1][j] == 1 {
-                        result -= 1
-                    }
-                    if j + 1 < columns && grid[i][j+1] == 1 {
-                        result -= 1
+                        result -= 2
                     }
                 }
             }
