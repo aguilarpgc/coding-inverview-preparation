3de780ac06c4a83adfe519bde3ce85fc38614377
Paul Aguilar <aguilarpgc@gmail.com>
2021-03-25 11:32:20 -0500

    594 improved

--- coding/leetcode/594.longest_harmonious_subsequence.swift
+++ coding/leetcode/594.longest_harmonious_subsequence.swift
@@ -1,16 +1,29 @@
 class Solution {
     func findLHS(_ nums: [Int]) -> Int {
+//         var countNum: [Int: Int] = [:]
+//         for num in nums {
+//             countNum[num, default: 0] += 1
+//         }
+        
+//         var answer = 0
+//         for (key, countCurrent) in countNum {
+//             guard let countNext = countNum[key + 1] else { continue }
+//             answer = max(answer, countCurrent + countNext)
+//         }
+        
+//         return answer
+        
         var countNum: [Int: Int] = [:]
+        var answer = 0
         for num in nums {
             countNum[num, default: 0] += 1
+            if let prevCount = countNum[num - 1] {
+                answer = max(answer, countNum[num, default: 0] + prevCount)
+            }
+            if let nextCount = countNum[num + 1] {
+                answer = max(answer, countNum[num, default: 0] + nextCount)
+            }
         }
-        
-        var answer = 0
-        for (key, countCurrent) in countNum {
-            guard let countNext = countNum[key + 1] else { continue }
-            answer = max(answer, countCurrent + countNext)
-        }
-        
         return answer
     }
 }
