ceca833db0cb6dd53a8ca169a1c536c056cedc09
Paul Aguilar <aguilarpgc@gmail.com>
2021-05-13 00:01:49 -0500

    improved 941

--- coding/leetcode/941.valid_mountain_array.swift
+++ coding/leetcode/941.valid_mountain_array.swift
@@ -1,25 +1,35 @@
 class Solution {
     func validMountainArray(_ arr: [Int]) -> Bool {
-        guard arr.count >= 3 else {
-            return false
+        // guard arr.count >= 3 else {
+        //     return false
+        // }
+        // guard arr[1] > arr[0] else {
+        //     return false
+        // }
+        // var up = true
+        // for i in 1..<arr.count {
+        //     if arr[i] == arr[i-1] {
+        //         return false
+        //     }
+        //     if arr[i] > arr[i-1] {
+        //         if !up {
+        //             return false
+        //         }
+        //     } else if up {
+        //             up.toggle()
+        //         }
+        //     }
+        // }
+        // return !up
+        guard arr.count > 2 else { return false }
+        var i = 0
+        var j = arr.count - 1
+        while i < arr.count - 1 && arr[i + 1] > arr[i] {
+            i += 1
         }
-        guard arr[1] > arr[0] else {
-            return false
+        while j > 0 && arr[j - 1] > arr[j] {
+            j -= 1
         }
-        var up = true
-        for i in 1..<arr.count {
-            if arr[i] == arr[i-1] {
-                return false
-            }
-            if arr[i] > arr[i-1] {
-                if !up {
-                    return false
-                }
-            } else if up {
-                    up.toggle()
-                }
-            }
-        }
-        return !up
+        return i == j && i > 0 && j < arr.count - 1
     }
 }
